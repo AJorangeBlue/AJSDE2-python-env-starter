ba535b466ff5e04588a04f41db8e23de8e07a3a2
AJorangeBlue <kingaiden17@gmail.com>
2025-06-25 23:18:52 +0000

    Fibonacci

---
+++ Lesson 10/fib.py
@@ -0,0 +1,15 @@
+# Fibonacci
+# 0, 1, 1, 3, 5, 8, 13, 21...
+
+def fib(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+print('testing fib function')
+print(fib(5))
+#fib(0)
+#print(fib(1))
