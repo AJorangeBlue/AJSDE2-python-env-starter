9019083b6504eeae098b8be31313e202e4f7f2f8
AJorangeBlue <kingaiden17@gmail.com>
2025-06-25 22:05:37 +0000

    Arac main

---
+++ Lesson4/1.1 & 1.2) Functions & add_One.py
@@ -0,0 +1,15 @@
+# 6/10/2025
+# 1.1 - Functions
+def sayHello(name): #definition
+  print(f'Hello {name}!')
+                             
+
+sayHello('Nia') # calling
+print('\n')
+
+#1.2 add_one
+def add_One(n):
+  print(n+1)
+  
+
+add_One(7)
--- Lesson6/aracnophonics1/main.py
+++ Lesson6/aracnophonics1/main.py
@@ -1,6 +1,9 @@
-#access libraries and py files 
+#access libraries and py files
+import time
+import os 
 import spiderDraw as sd
 import functions as md
+
 
 #Initialize variables and setup 
 #Need to keep track of correct letters, incorrect letters and tries
@@ -13,19 +16,55 @@ game = True
 #Make a list of the spider drawings
 spiderList = [sd.spider_0, sd.spider_1, sd.spider_2, sd.spider_3, sd.spider_4, sd.spider_5, sd.spider_6]
 
+limit = len(spiderList) - 1 
 
-#Print intro statements (welcome to game, etc)
-
-
-
-#generate a random word from word list
-word = md.generate_word()  
+def play_game():
+  word = md.generate_word()   
+  correct = []  #List of correct letters guessed
+  incorrect = []  #List of incorrect letters guessed
+  tries = len(incorrect)   #Number of incorrect guesses
+  #game = True
+  #tries == limit 
 
 
 
 #Game Loop
-while game: 
+#run_game()
+#while keep_playing
+# 
+while game:
+  time.sleep(1)
+  os.system('clear') # clear screen
+
+  print(md.print_word(word, correct))
+  md.print_wrong_guesses(incorrect)  
   md.print_spider(tries,spiderList)
+  guess = input('Guess a letter:\n')
+  correct, incorrect = md.check_word(guess, correct, incorrect)
+
+
+
+
+
+
+  '''
+  if not md.is_guess_vaild(guess):
+    print('You only need one letter. Enter again\n')
+    continue
+
+  if md.already_guessed(guess, correct, incorrect):
+    print('Already guessed that letter. Try again!')
+    continue
+
+  if md.is_correct_guess(guess, word):
+    correct.append(guess)
+    print('Yep, correct!')
+  else:
+    incorrect.append(guess)
+    print('Incorrect, my friend!')
+
+  break
+  '''
 
   #This is where you'll call all of your functions. Just need to decide the proper order.
 
