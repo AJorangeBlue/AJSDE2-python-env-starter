ec8c5b208718b672f0149c0d3e776b9da9ff0d1e
AJorangeBlue <kingaiden17@gmail.com>
2025-07-09 22:02:38 +0000

    2025 lesson

---
+++ .devcontainer/Lesson12/2025.py
@@ -0,0 +1,14 @@
+for i, fruit in enumerate(['apple', 'orange']):
+    if  i % 2 > 0:
+        print(fruit)
+
+red = '31'
+green = '25'
+yellow = '22'
+blue = '34'
+
+print(f'\033[31mHello World!\033[0m')
+print(f'\033[32mHello World!\033[0m')
+print(f'\033[33mHello World!\033[0m')
+print(f'\033[34mHello World!\033[0m')
+
